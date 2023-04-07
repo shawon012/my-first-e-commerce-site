1274d999464b998b5508157b358a111e03ae7079
shawon012 <shawon.shakib01@gmail.com>
2023-04-07 22:59:48 +0600

    local storage work is going on

--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import './Shop.css';
 import Products from '../Products/Products';
 import Cart from '../Cart/Cart';
+import { addToDb } from '../../utilities/fakedb';
 
 const Shop = () => {
     const [products, setProducts] = useState([]);
@@ -16,6 +17,7 @@ const Shop = () => {
     const handleAddToCart = (product) =>{
         const newCart = [...cart, product];
         setCart(newCart);
+        addToDb(product.id)
     }
 
     return (
