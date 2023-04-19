53ce730ab1930c8babb0ea08906469e3e048bb87
shawon012 <shawon.shakib01@gmail.com>
2023-04-19 23:08:40 +0600

    login and signin form done

--- src/components/Signup/Signup.jsx
+++ src/components/Signup/Signup.jsx
@@ -3,9 +3,24 @@ import './Signup.css'
 
 const Signup = () => {
     return (
-        <div>
-            <h2>Please SignUp!!!</h2>
-        </div>
+        <div className='form-container'>
+            <h3 className='form-title'>SignUp</h3>
+            <form>
+                <div className="form-control">
+                    <label htmlFor="email">Email</label>
+                    <input type="email" name="email" id="" required/>
+                </div>
+                <div className="form-control">
+                    <label htmlFor="password">Password</label>
+                    <input type="password" name="password" id="" required/>
+                </div>
+                <div className="form-control">
+                    <label htmlFor="password">Confirm Password</label>
+                    <input type="password" name="confirm" id="" required/>
+                </div>
+                <input className='btn-submit' type="submit" value="SignUp" />
+            </form>
+        </div> 
     );
 };
 
